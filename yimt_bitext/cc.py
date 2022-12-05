52ad5eb8f0b629fcfdf611713bedca315f93d256
liuxf_hust <hlp_ai@sina.com>
2022-12-05 16:32:07 +0800

    Support download progress bar.

--- yimt_bitext/cc.py
+++ yimt_bitext/cc.py
@@ -18,6 +18,27 @@ def download(url, local_fn):
     open(local_fn, 'wb').write(response.content)
 
 
+def download_bar(url, filepath):
+    start = time.time()
+    response = requests.get(url, stream=True)
+    size = 0
+    chunk_size = 4096 * 16
+    content_size = int(response.headers['content-length'])
+    try:
+        if response.status_code == 200:
+            print('开始下载,[文件大小]:{size:.2f} MB'.format(size=content_size / 1024 / 1024))
+            with open(filepath, 'wb') as file:
+                for data in response.iter_content(chunk_size=chunk_size):
+                    file.write(data)
+                    size += len(data)
+                    print('\r' + '[下载进度]:%s%.2f%%' % (
+                        '>' * int(size * 50 / content_size), float(size / content_size * 100)), end=' ')
+        end = time.time()
+        print('完成！用时: %.2f秒' % (end - start))
+    except Exception:
+        pass
+
+
 def ungzip(zip_fn, unzip_fn):
     print("Unziping ", zip_fn)
     g = gzip.GzipFile(mode="rb", fileobj=open(zip_fn, 'rb'))
@@ -40,6 +61,4 @@ wp = get_wet_paths("CC-MAIN-2022-40")
 for p in wp:
     print(p)
 
-start = time.time()
-download(wp[4], "4.gz")
-print(time.time() - start)
+download_bar(wp[4], "4.gz")

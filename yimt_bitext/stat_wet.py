776316b2a2c5aeef859f7d84a9b1a7f66d6ff752
liuxf_hust <hlp_ai@sina.com>
2022-12-05 19:15:38 +0800

    Refactor stat_wet.py.

--- yimt_bitext/stat_wet.py
+++ yimt_bitext/stat_wet.py
@@ -3,8 +3,7 @@ import argparse
 import json
 import os
 
-from warcio.archiveiterator import ArchiveIterator
-from urllib.parse import urlparse
+from yimt_bitext.cc import count_lang
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
@@ -12,7 +11,7 @@ if __name__ == "__main__":
     argparser.add_argument("--stat_path", type=str, default="./stat_host.json", help="stat file")
     args = argparser.parse_args()
 
-    wet_path = args.wet_path # r"D:\dataset\text\cc22-40\CC-MAIN-20220924151538-20220924181538-00001.warc.wet"
+    wet_path = args.wet_path
     stat_path = args.stat_path
 
     host2lang2len = {}
@@ -24,49 +23,7 @@ if __name__ == "__main__":
 
     print("# hosts: ", len(host2lang2len))
 
-    new_hosts = 0
-
-    with open(wet_path, 'rb') as stream:
-        i = 0
-        for record in ArchiveIterator(stream):
-            if record.rec_type == 'conversion':
-                langs = record.rec_headers.get_header("WARC-Identified-Content-Language")
-                url = record.rec_headers.get_header("WARC-Target-URI")
-                content_len = int(record.rec_headers.get_header("Content-Length"))
-                if langs is not None:
-                    langs = langs.split(",")
-                else:
-                    langs = []
-
-                u = urlparse(url)
-                host = u.netloc
-
-                if host not in host2lang2len:
-                    host2lang2len[host] = {}
-                    new_hosts += 1
-
-                lang2len = host2lang2len[host]
-
-                for lang in langs:
-                    if lang in lang2len:
-                        lang2len[lang] += content_len
-                    else:
-                        lang2len[lang] = content_len
-
-                # print(host, url, langs, content_len)
-                # print(record.rec_headers.get_header("WARC-Identified-Content-Language"),
-                #       record.rec_headers.get_header("WARC-Target-URI"),
-                #       record.rec_headers.get_header("Content-Length"))
-                # print(record.content_stream().read().decode("utf-8"))
-                # print()
-            i += 1
-            # if i >= 10000:
-            #     break
-
-            if i % 10000 == 0:
-                print(i)
-            # if record.rec_type == 'response':
-            #     print(record.rec_headers.get_header('WARC-Target-URI'))
+    new_hosts = count_lang(wet_path, host2lang2len)
 
     print("# new hosts: ", new_hosts)
     print("# hosts: ", len(host2lang2len))

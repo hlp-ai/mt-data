424a89213a61d6f29fb69f13a7eaa11e6863ee67
liuxf_hust <hlp_ai@sina.com>
2022-12-05 19:15:11 +0800

    Add stat_wets.py.

---
+++ yimt_bitext/stat_wets.py
@@ -0,0 +1,42 @@
+"""1. Count the total lengths of text with different languages for hosts from WET file of CommonCrawl"""
+import argparse
+import json
+import os
+
+from yimt_bitext.cc import count_lang, get_wet_name, download_progress, ungzip, cc_base_url
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("--wet_paths", default="./wet.paths", help="WET list file")
+    argparser.add_argument("--stat_path", default="./stat_host.json", help="stat file")
+    args = argparser.parse_args()
+
+    wet_paths = args.wet_paths
+    stat_path = args.stat_path
+
+    host2lang2len = {}
+
+    if os.path.exists(stat_path):
+        with open(stat_path, encoding="utf-8") as f:
+            print("Loading stat file for updating...")
+            host2lang2len = json.load(f)
+
+    print("# hosts: ", len(host2lang2len))
+
+    with open(wet_paths, encoding="utf-8") as f:
+        for url in f:
+            url = cc_base_url + url.strip()
+            print("Counting from ", url)
+            wet_gz_path, wet_path = get_wet_name(url)
+            download_progress(url, wet_gz_path)
+            ungzip(wet_gz_path, wet_path)
+
+            new_hosts = count_lang(wet_path, host2lang2len)
+
+            print("# new hosts: ", new_hosts)
+            print("# hosts: ", len(host2lang2len))
+
+    print("Writing stat file...")
+    with open(stat_path, "w", encoding="utf-8") as stream:
+        json.dump(host2lang2len, stream)
+

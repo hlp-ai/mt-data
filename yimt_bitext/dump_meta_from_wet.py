1ac37c2de399867dfd020a9bc254aaf124b148b0
Xiaofeng Liu <liuxf@hust.edu.cn>
2022-12-16 17:32:01 +0800

    Add dump_meta_from_wet.py.

---
+++ yimt_bitext/dump_meta_from_wet.py
@@ -0,0 +1,11 @@
+import argparse
+
+from yimt_bitext.cc import dump_metadata_wet
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("--wet_path", type=str, required=True, help="WET file")
+    argparser.add_argument("--output", type=str, default=None, help="Output metadata file")
+    args = argparser.parse_args()
+
+    dump_metadata_wet(args.wet_path, args.output)

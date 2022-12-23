76b53ecdf50a94677fa71fee2604b2abcdc7097c
Xiaofeng Liu <liuxf@hust.edu.cn>
2022-12-23 15:32:28 +0800

    Merge metadata files for stating.

--- yimt_bitext/stat_from_meta.py
+++ yimt_bitext/stat_from_meta.py
@@ -12,15 +12,27 @@ if __name__ == "__main__":
 
     meta_files = glob.glob(os.path.join(args.meta_dir, "*.meta"))
 
+    stat_by_host = {}
+    stat_by_domain = {}
+
     for f in meta_files:
         print("Stating from metadata file ", f)
         s_by_host, s_by_domain = stat_from_meta(f)
 
-        stat_host_fn = os.path.join(os.path.dirname(f), os.path.basename(f) + ".host.json")
-        stat_domain_fn = os.path.join(os.path.dirname(f), os.path.basename(f) + ".domain.json")
+        print("# of hosts: ", len(s_by_host), " # of domains: ", len(s_by_domain))
 
-        with open(stat_host_fn, "w", encoding="utf-8") as stream:
-            json.dump(s_by_host, stream)
+        stat_by_host = merge_k2dict(stat_by_host, s_by_host)
+        stat_by_domain = merge_k2dict(stat_by_domain, s_by_domain)
 
-        with open(stat_domain_fn, "w", encoding="utf-8") as stream:
-            json.dump(s_by_domain, stream)
+        print("# of hosts: ", len(stat_by_host), " # of domains: ", len(stat_by_domain))
+
+    print("# of hosts: ", len(stat_by_host), " # of domains: ", len(stat_by_domain))
+
+    stat_host_fn = os.path.join(args.meta_dir, "stat_by_host.json")
+    stat_domain_fn = os.path.join(args.meta_dir, "stat_by_domain.json")
+
+    with open(stat_host_fn, "w", encoding="utf-8") as stream:
+        json.dump(stat_by_host, stream)
+
+    with open(stat_domain_fn, "w", encoding="utf-8") as stream:
+        json.dump(stat_by_domain, stream)

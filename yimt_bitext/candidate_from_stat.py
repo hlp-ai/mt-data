fb5b6101ea7fda93fc0226ad68d65179cf83a12b
Xiaofeng Liu <liuxf@hust.edu.cn>
2023-02-17 15:51:45 +0800

    Improve functions.

--- yimt_bitext/candidate_from_stat.py
+++ yimt_bitext/candidate_from_stat.py
@@ -1,25 +1,30 @@
 """3. Output bilingual hosts/domains based on stat data"""
 import argparse
 import json
+import os
 
 from yimt_bitext.cc import update_k2set
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
     argparser.add_argument("--stat_path", type=str, required=True, help="stat file")
-    argparser.add_argument("--out_path", type=str, default="./multihost2langs.json", help="output file path")
     args = argparser.parse_args()
 
     stat_path = args.stat_path
-    output_path = args.out_path
+    multihost2langs_path = os.path.join(os.path.dirname(stat_path), "multihost2langs.json")
 
+    host2lang2len = {}
+    print("Loading stat file...")
     with open(stat_path, encoding="utf-8") as f:
-        print("Loading stat file...")
         host2lang2len = json.load(f)
 
     print("# network loc: ", len(host2lang2len))
 
     multihost2langs = {}
+    if os.path.exists(multihost2langs_path):
+        print("Loading stat file for updating...")
+        with open(multihost2langs_path, encoding="utf-8") as f:
+            multihost2langs = json.load(f)
 
     for netloc, lang2len in host2lang2len.items():
         # TODO: judgement by ratio of lengths of text of different languages
@@ -27,7 +32,7 @@ if __name__ == "__main__":
             for lang, txt_len in lang2len.items():
                 update_k2set(multihost2langs, netloc, lang)
 
-    with open(output_path, "w", encoding="utf-8") as stream:
+    with open(multihost2langs_path, "w", encoding="utf-8") as stream:
         json.dump(multihost2langs, stream)
 
     print("# of multilingual host:", len(multihost2langs))
--- yimt_bitext/stat_from_meta.py
+++ yimt_bitext/stat_from_meta.py
@@ -1,37 +1,40 @@
-""""2. Calculate statistics from metadata file"""
+""""2. Calculate statistics on languages of text from metadata file"""
 import argparse
 import glob
 import json
 import os
+import shutil
 
-from yimt_bitext.cc import stat_from_meta, merge_k2dict, merge_k2set
+from yimt_bitext.cc import stat_from_meta, merge_k2dict
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
     argparser.add_argument("--meta_dir", required=True, help="Directory of metadata file")
     args = argparser.parse_args()
 
-    meta_files = glob.glob(os.path.join(args.meta_dir, "*.meta"))
+    meta_dir = args.meta_dir
+
+    processed_meta_dir = os.path.join(meta_dir, "processed_meta")
+    if not os.path.exists(processed_meta_dir):
+        os.mkdir(processed_meta_dir)
 
     host2lang2len = {}
-
     host2lang2len_fn = os.path.join(args.meta_dir, "host2lang2len.json")
-
-    update = False
-    if update:
+    if os.path.exists(host2lang2len_fn):
         print("Loading existing stat for updating...")
         with open(host2lang2len_fn, encoding="utf-8") as stream:
             host2lang2len = json.load(stream)
 
+    meta_files = glob.glob(os.path.join(meta_dir, "*.meta"))
     for f in meta_files:
         print("Stating from metadata file ", f)
         host2lang2len_local = stat_from_meta(f)
-
         print("  # of hosts found: ", len(host2lang2len_local))
 
         host2lang2len = merge_k2dict(host2lang2len, host2lang2len_local)
-
         print("  # of hosts after merging: ", len(host2lang2len))
+
+        shutil.move(f, processed_meta_dir)
 
     with open(host2lang2len_fn, "w", encoding="utf-8") as stream:
         json.dump(host2lang2len, stream)

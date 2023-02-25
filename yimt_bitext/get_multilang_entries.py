5d462c1d243c305991b94fa8af06ad3a9d9cd19c
Xiaofeng Liu <liuxf@hust.edu.cn>
2023-02-25 14:29:00 +0800

    Update list of multilingual entries incrementally.

--- yimt_bitext/get_multilang_entries.py
+++ yimt_bitext/get_multilang_entries.py
@@ -13,7 +13,17 @@ if __name__ == "__main__":
     with open(multihost2langs_fn, encoding="utf-8") as stream:
         multihost2langs = json.load(stream)
 
+    entries_path = os.path.join(os.path.dirname(multihost2langs_fn), "entires_tocrawl.txt")
+
     entries = set()
+    if os.path.exists(entries_path):
+        print("Loading entries for updating...")
+        with open(entries_path, encoding="utf-8") as f:
+            for entry in f:
+                entries.add(entry.strip())
+
+    print("# of entries found:", len(entries))
+
     lang1, lang2 = args.langs
 
     entries_found = 0
@@ -21,16 +31,15 @@ if __name__ == "__main__":
     total = 0
 
     for host, langs in multihost2langs.items():
-        if lang1 in langs and lang2 in langs:
+        if lang1 in langs and lang2 in langs and host not in entries:
             entries.add(host)
             entries_found += 1
 
         total += 1
         if total % report_interval == 0:
-            print(total, entries_found)
-    print(total, entries_found)
+            print("{} entires scanned, {} new multilingual entries found".format(total, entries_found))
+    print("{} entires scanned, {} new multilingual entries found".format(total, entries_found))
 
-    out_path = os.path.join(os.path.dirname(multihost2langs_fn), "urls_tocrawl.txt")
-    with open(out_path, "w", encoding="utf-8") as stream:
+    with open(entries_path, "w", encoding="utf-8") as stream:
         for url in entries:
             stream.write(url + "\n")

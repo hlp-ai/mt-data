3d7979b77fd53b30c442234dbad5e1922c10680c
liuxf_hust <hlp_ai@sina.com>
2023-04-14 15:32:24 +0800

    Improve test for LangStat.

--- yimt_bitext/web/tests/langstat_test.py
+++ yimt_bitext/web/tests/langstat_test.py
@@ -3,10 +3,10 @@ from yimt_bitext.web.base import BasicLangStat
 if __name__ == "__main__":
     stat_f = "./d2h2lang2len.json"
     lang_stat = BasicLangStat(stat_f)
-    lang_stat.update("http://www.abc.com/", {"zh": 100})
-    assert lang_stat.stat_by_host("http://www.abc.com/") == {"zh": 100}
 
+    lang_stat.update("http://www.abc.com/", {"zh": 100, "en": 30})
     lang_stat.update("http://en.abc.com/", {"en": 100})
+
     assert lang_stat.size() == 1
     assert len(lang_stat.hosts("abc.com")) == 2
 
@@ -14,6 +14,6 @@ if __name__ == "__main__":
 
     lang_stat = BasicLangStat(stat_f)
     lang2len = lang_stat.lang2len_by_domain("abc.com")
-    assert lang2len["zh"] == 100
+    assert lang2len["en"] == 130
     lang2len = lang_stat.lang2len_by_host("http://en.abc.com/")
     assert lang2len["en"] == 100

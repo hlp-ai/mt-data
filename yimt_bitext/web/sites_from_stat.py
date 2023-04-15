2d6c6aed56bef1ff60b0d155c9f8a721443607cc
Xiaofeng Liu <liuxf@hust.edu.cn>
2023-04-15 16:12:22 +0800

    Output results into file.

--- yimt_bitext/web/sites_from_stat.py
+++ yimt_bitext/web/sites_from_stat.py
@@ -6,5 +6,8 @@ if __name__ == "__main__":
     stat_f = sys.argv[1]
     langs = sys.argv[2].split(",")
     lang_stat = BasicLangStat(stat_f)
-    for host in lang_stat.hosts_for_langs(langs):
-        print(host)
+    out_f = "sites-" + "-".join(langs) + ".txt"
+
+    with open(out_f, "w", encoding="utf-8") as f:
+        for host in lang_stat.hosts_for_langs(langs):
+            f.write(host + "\n")

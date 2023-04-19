51baae0a6e6658e038fe2b0b33b5ef4dd82768d4
Xiaofeng Liu <liuxf@hust.edu.cn>
2023-04-19 16:20:50 +0800

    Add argument for crawl.py.

--- yimt_bitext/web/crawl.py
+++ yimt_bitext/web/crawl.py
@@ -1,47 +1,51 @@
 """5. Crawl multilingual entries"""
+import sys
+
 from yimt_bitext.web.base import BasicUrlsToCrawl, BasicUrlsCrawled, BasicCrawler, BasicPageParser, \
     BasicSentenceSplitter, BasicLangID, BasicSentenceRepo, SentenceRepoFile
 from yimt_bitext.web.web import URL
 
 
-to_crawl_fn = "../CC-MAIN-2022-40/urls_tocrawl.txt"
-to_crawl = BasicUrlsToCrawl(to_crawl_fn)
-crawled = BasicUrlsCrawled()
-crawler = BasicCrawler()
-parser = BasicPageParser()
-sentence_splitter = BasicSentenceSplitter()
-langid = BasicLangID()
-sent_repo = SentenceRepoFile(accepted_langs=["zh", "en", "ko"])
+if __name__ == "__main__":
+    to_crawl_fn = sys.argv[1]  # "../CC-MAIN-2022-40/urls_tocrawl.txt"
 
-while True:
-    url = to_crawl.next()
-    if url is None:
-        break
-    print("Fetching", url)
-    html_content = crawler.crawl(url)
-    if html_content is not None:
-        print("Parsing", url)
-        txt, outlinks = parser.parse(html_content, url)
+    to_crawl = BasicUrlsToCrawl(to_crawl_fn)
+    crawled = BasicUrlsCrawled()
+    crawler = BasicCrawler()
+    parser = BasicPageParser()
+    sentence_splitter = BasicSentenceSplitter()
+    langid = BasicLangID()
+    sent_repo = SentenceRepoFile(accepted_langs=["zh", "en", "ko"])
 
-        sentences = sentence_splitter.split(txt)
-        lang2sentenes = {}
-        for s in sentences:
-            lang = langid.detect(s)
-            if lang in lang2sentenes:
-                lang2sentenes[lang].append(s)
-            else:
-                lang2sentenes[lang] = [s]
+    while True:
+        url = to_crawl.next()
+        if url is None:
+            break
+        print("Fetching", url)
+        html_content = crawler.crawl(url)
+        if html_content is not None:
+            print("Parsing", url)
+            txt, outlinks = parser.parse(html_content, url)
 
-        sent_repo.store(lang2sentenes)
-        print(sent_repo)
+            sentences = sentence_splitter.split(txt)
+            lang2sentenes = {}
+            for s in sentences:
+                lang = langid.detect(s)
+                if lang in lang2sentenes:
+                    lang2sentenes[lang].append(s)
+                else:
+                    lang2sentenes[lang] = [s]
 
-        # crawl in-site
-        u = URL(url)
-        site = u.scheme + "://" + u.netloc + "/"
-        outlinks = list(filter(lambda ol: ol.startswith(site), outlinks))
-        for ol in outlinks:
-            to_crawl.add(ol)
+            sent_repo.store(lang2sentenes)
+            print(sent_repo)
 
-        crawled.add(url)
+            # crawl in-site
+            u = URL(url)
+            site = u.scheme + "://" + u.netloc + "/"
+            outlinks = list(filter(lambda ol: ol.startswith(site), outlinks))
+            for ol in outlinks:
+                to_crawl.add(ol)
 
-    print(len(crawled), "crawled,", len(to_crawl), "to crawl.")
+            crawled.add(url)
+
+        print(len(crawled), "crawled,", len(to_crawl), "to crawl.")

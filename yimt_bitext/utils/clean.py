cf098ea07a78ec7986511929ff942e32d7674053
Xiaofeng Liu <liuxf@hust.edu.cn>
2023-05-07 16:21:15 +0800

    Add clean.py.

---
+++ yimt_bitext/utils/clean.py
@@ -0,0 +1,46 @@
+import unicodedata
+
+
+def is_whitespace(char):
+    """Checks whether `chars` is a whitespace character."""
+    # \t, \n, and \r are technically contorl characters but we treat them
+    # as whitespace since they are generally considered as such.
+    if char == " " or char == "\t" or char == "\n" or char == "\r":
+        return True
+    cat = unicodedata.category(char)
+    if cat == "Zs":
+        return True
+
+    return False
+
+
+def is_control(char):
+    """Checks whether `chars` is a control character."""
+    # These are technically control characters but we count them as whitespace
+    # characters.
+    if char == "\t" or char == "\n" or char == "\r":
+        return False
+    cat = unicodedata.category(char)
+    if cat in ("Cc", "Cf"):
+        return True
+    return False
+
+
+def clean_text(text):
+    """Performs invalid character removal and whitespace cleanup on text."""
+    output = []
+    for char in text:
+        cp = ord(char)
+        if cp == 0 or cp == 0xfffd or is_control(char):
+            continue
+        if is_whitespace(char):
+            output.append(" ")
+        else:
+            output.append(char)
+    return "".join(output)
+
+
+if __name__ == "__main__":
+    print(clean_text("提⁠供⁠四⁠項⁠保⁠障⁠：房源預訂保障如果在罕見情況下，"))
+    print(clean_text("Last Updated: 2023 年 1 月 25 日Thank you for using Airbnb!"))
+    print(clean_text("�u���=:�����"))

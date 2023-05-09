e38172162c7450637effe534d7dbdf3e7c107b9b
liuxf_hust <hlp_ai@sina.com>
2023-05-09 13:51:44 +0800

    Store sites file as json file.

--- yimt_bitext/web/sites_from_stat.py
+++ yimt_bitext/web/sites_from_stat.py
@@ -1,5 +1,7 @@
 """3. Get multilingual domains for given languages"""
+import json
 import sys
+from collections import defaultdict
 
 from yimt_bitext.web.base import BasicLangStat
 from yimt_bitext.web.url_language import UrlLanguage
@@ -8,18 +10,22 @@ if __name__ == "__main__":
     stat_f = sys.argv[1]
     langs = sys.argv[2].split(",")
     lang_stat = BasicLangStat(stat_f)
-    out_f = "sites-" + "-".join(langs) + ".txt"
+    out_f = "sites-" + "-".join(langs) + ".json"
 
     filter = True
     url_language = UrlLanguage()
     normalized_langs = [url_language.normalize_lang_code(lang) for lang in langs]
 
+    domain2hosts_langs = defaultdict(list)
+    for domain, hosts in lang_stat.hosts_for_langs(langs):
+        for h in hosts:
+            lang = url_language.find_language(h)
+            if lang and lang not in normalized_langs:
+                print(h, "filtered")
+                continue
+            domain2hosts_langs[domain].append(h)
+
     with open(out_f, "w", encoding="utf-8") as f:
-        for domain, hosts in lang_stat.hosts_for_langs(langs):
-            f.write("*" + domain + "\n")
-            for h in hosts:
-                lang = url_language.find_language(h)
-                if lang and lang not in normalized_langs:
-                    print(h, "filtered")
-                    continue
-                f.write(h + "\n")
+        json.dump(domain2hosts_langs, f)
+
+    print("# of domains: {}".format(len(domain2hosts_langs)))

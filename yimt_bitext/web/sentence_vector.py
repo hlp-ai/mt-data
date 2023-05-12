924266ef8c408b383d6ab69346b4b54b39d5f175
liuxf_hust <hlp_ai@sina.com>
2023-05-12 11:21:39 +0800

    Refactor sentence_vector.py

--- yimt_bitext/web/sentence_vector.py
+++ yimt_bitext/web/sentence_vector.py
@@ -1,8 +1,8 @@
 import numpy as np
-from annoy import AnnoyIndex
 
 
-class SentenceVectorization:   #计算文本或文本列表的向量表示
+class SentenceVectorization:
+    """计算文本或文本列表的向量表示"""
 
     def get_vector(self, text):
         raise NotImplementedError
@@ -17,7 +17,7 @@ class NaiveSentenceVectorization(SentenceVectorization):
             return np.array([[1.0, 2.0], [1.0, 2.0]])
 
 
-class SentenceVectorizationLaBSE(SentenceVectorization):  #句子嵌入
+class SentenceVectorizationLaBSE(SentenceVectorization):
     def __init__(self, model_url, max_seq_length=48):
         self.max_seq_length = max_seq_length
         self.labse_model, labse_layer = self._get_model(model_url, max_seq_length)
@@ -69,12 +69,14 @@ class SentenceVectorizationLaBSE(SentenceVectorization):  #句子嵌入
 
         return np.array(input_ids_all), np.array(input_mask_all), np.array(segment_ids_all)
 
-    def get_vector(self, text):     #输出句子的嵌入向量
+    def get_vector(self, text):
+        #输出句子的嵌入向量
         input_ids, input_mask, segment_ids = self._create_input(text, self.tokenizer, self.max_seq_length)
         return self.labse_model([input_ids, input_mask, segment_ids])
 
 
-class VectorSimilarity:   #计算向量或向量列表间的相似性分数
+class VectorSimilarity:
+    """计算向量或向量列表间的相似性分数"""
 
     def get_score(self, vec1, vec2):
         raise NotImplementedError
@@ -97,56 +99,67 @@ class VectorSimilarityCosine(VectorSimilarity):
         return num / denom
 
 
-class VectorSimilarityMargin(VectorSimilarity):    #实现：边缘分数
-    def __init__(self, u1, u2, k):      #u1,u2是annoy索引树的索引，k是计算边缘分数式中的k值
+class VectorSimilarityMargin(VectorSimilarity):
+    #实现：边缘分数
+    def __init__(self, index1, index2, k):
+        #index1, index2是annoy索引树的索引，k是计算边缘分数式中的k值
         self.k = k
-        self.u1 = u1
-        self.u2 = u2
+        self.index1 = index1
+        self.index2 = index2
+        self.cos_sim = VectorSimilarityCosine()
 
-    def get_score(self, vec1, vec2):      #计算np向量vec1, vec2之间的边缘分数
-        cos_sim = VectorSimilarityCosine()
-        cos_xy = cos_sim.get_score(vec1, vec2)
-        src_neighbors = self.u1.get_nns_by_vector(vec1, self.k)
-        tar_neighbors = self.u2.get_nns_by_vector(vec2, self.k)
+    def get_score(self, vec1, vec2):
+        #计算np向量vec1, vec2之间的边缘分数
+        cos_xy = self.cos_sim.get_score(vec1, vec2)
+        src_neighbors = self.index1.get_nns_by_vector(vec1, self.k)
+        tar_neighbors = self.index2.get_nns_by_vector(vec2, self.k)
         denominator = 0
         for v in src_neighbors:
-            denominator += cos_sim.get_score(vec1, self.u1.get_item_vector(v))
+            denominator += self.cos_sim.get_score(vec1, self.index1.get_item_vector(v))
         for v in tar_neighbors:
-            denominator += cos_sim.get_score(vec2, self.u2.get_item_vector(v))
+            denominator += self.cos_sim.get_score(vec2, self.index2.get_item_vector(v))
         denominator = denominator / self.k / 2
         margin = cos_xy / denominator
         return margin
 
 
-def init_tree(sentence_embeddings, dim=48, num=10, annoy_dir='source.ann'):   #建立annoy索引树,num为树个数，load_dir为索引树存储路径
+def build_vec_index(sentence_embeddings, annoy_dir, dim=48, tree_num=10):
+    #建立annoy索引树,num为树个数，load_dir为索引树存储路径
+    from annoy import AnnoyIndex
+
     t = AnnoyIndex(dim, 'angular')
     i = 0
     for s in sentence_embeddings:
         t.add_item(i, s)
         i = i + 1
-    t.build(num)
+    t.build(tree_num)
     t.save(annoy_dir)
 
-"""
-    #以下可供测试使用
-    if __name__ == '__main__':
 
-    src_embeddings = np.random.normal(0, 1, (100000, 3))  
-    tar_embeddings = np.random.normal(0, 1, (100000, 3))
-    init_tree(src_embeddings, 3, 6, 'source.ann')
-    init_tree(tar_embeddings, 3, 6, 'target.ann')
+def load_vec_index(annoy_dir, dim=48):
+    from annoy import AnnoyIndex
+
+    vec_index = AnnoyIndex(dim, 'angular')
+    vec_index.load(annoy_dir)
+
+    return vec_index
+
+
+if __name__ == '__main__':
     dim = 3
-    u1 = AnnoyIndex(dim, 'angular')
-    u1.load('source.ann')
-    u2 = AnnoyIndex(dim, 'angular')
-    u2.load('target.ann')
-   
-    get_margin = VectorSimilarityMargin(u1,u2,8)
+    src_embeddings = np.random.normal(0, 1, (100000, dim))
+    tar_embeddings = np.random.normal(0, 1, (100000, dim))
+    build_vec_index(src_embeddings, 'source.ann', dim, 6)
+    build_vec_index(tar_embeddings, 'target.ann', dim, 6)
+
+    vec_index1 = load_vec_index('source.ann', dim)
+    vec_index2 = load_vec_index('target.ann', dim)
+
+    k = 8
+    get_margin = VectorSimilarityMargin(vec_index1, vec_index2, k)
 
     v1 = np.array([0, 3, 0])
     v2 = np.array([0, -3, 0])
 
     s=get_margin.get_score(v1,v2)
     print(s)
-
-"""

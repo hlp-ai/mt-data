2e62a067e0b7416466b8433745858801d015a73e
Xiaofeng Liu <liuxf@hust.edu.cn>
2023-05-21 15:19:07 +0800

    Add progress info for scoring sentence pairs.

--- yimt_bitext/web/score_segments.py
+++ yimt_bitext/web/score_segments.py
@@ -1,3 +1,4 @@
+"""6. Score sentence pairs"""
 import sys
 
 from yimt_bitext.web.sentence_vector import VectorSimilarityCosine, SentenceVectorizationLaBSE
@@ -17,15 +18,25 @@ if __name__ == "__main__":
         for s in f1:
             segs1.append(s.strip())
 
+    n1 = len(segs1)
+
     with open(fn2, encoding="utf-8") as f2:
         for s in f2:
             segs2.append(s.strip())
 
+    n2 = len(segs2)
+
+    print("File-1: {}, File-2: {}".format(n1, n2))
+
     block = 16
+    t1 = 0
+    t2 = 0
 
     with open(out, "w", encoding="utf-8") as of:
         for i in range(0, len(segs1), block):
             b1 = segs1[i:i+block]
+            t1 += len(b1)
+            t2 = 0
             for j in range(0, len(segs2), block):
                 b2 = segs2[j:j+block]
 
@@ -36,3 +47,6 @@ if __name__ == "__main__":
                 for m, s in enumerate(b1, 0):
                     for n, t in enumerate(b2, 0):
                         of.write("{:.4f} {} {}\n".format(scores[m, n], s, t))
+
+                t2 += len(b2)
+                print("File-1: {}, File-2: {}".format(t1, t2))

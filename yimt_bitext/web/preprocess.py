1c41369fd42063a72687881b94e2724bb44855b8
Xiaofeng Liu <liuxf@hust.edu.cn>
2023-05-22 13:54:46 +0800

    Split paragraph into sentences before deduping.

--- yimt_bitext/web/preprocess.py
+++ yimt_bitext/web/preprocess.py
@@ -4,14 +4,22 @@ import sys
 
 from yimt_bitext.utils.clean import clean_file
 from yimt_bitext.utils.dedup import dedup_file
+from yimt_bitext.utils.text_splitter import split_sent_file
 
 if __name__ == "__main__":
     domain_dir = sys.argv[1]
     sents_dir = os.path.join(domain_dir, "lang2sents")
 
     for f in os.listdir(sents_dir):
+        if not f.endswith(".txt"):
+            continue
+        lang = os.path.basename(f)
         f = os.path.join(sents_dir, f)
-        print("Cleaning {}".format(f))
+        print("Cleaning {}...".format(f))
         out = clean_file(f)
-        print("Deduping {}".format(out))
+
+        print("Splitting {}...".format(out))
+        out = split_sent_file(out, lang=lang)
+
+        print("Deduping {}...".format(out))
         dedup_file(out)

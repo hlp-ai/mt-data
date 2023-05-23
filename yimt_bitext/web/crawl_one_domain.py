74ef3c904f81d5f49bd7976b57892f352328e37e
liuxf_hust <hlp_ai@sina.com>
2023-05-23 15:43:06 +0800

    Add argument parsing for crawl_one_domain.py.

--- yimt_bitext/web/crawl_one_domain.py
+++ yimt_bitext/web/crawl_one_domain.py
@@ -1,11 +1,16 @@
 """4. Crawl multilingual domain"""
+import argparse
 import sys
 
 from yimt_bitext.web.crawl import crawl_domain
 
 if __name__ == "__main__":
-    path = sys.argv[1]
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("--domain_dir", default=None, help="Directory for domain")
+    argparser.add_argument("--langs", required=True, help="Language list seperated with comma")
+    args = argparser.parse_args()
 
-    accepted_langs = ["zh", "en", "ko"]
+    accepted_langs = args.langs.split(",")
+    path = sys.domain_dir
 
     crawl_domain(path, accepted_langs)

5c86cc8ab7583d72f468b85a853c1f3436b08d95
liuxf_hust <hlp_ai@sina.com>
2023-05-23 15:55:29 +0800

    Improve help text for argument.

--- yimt_bitext/web/crawl_one_domain.py
+++ yimt_bitext/web/crawl_one_domain.py
@@ -6,7 +6,7 @@ from yimt_bitext.web.crawl import crawl_domain
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
     argparser.add_argument("--domain_dir", default=None, help="Directory for domain")
-    argparser.add_argument("--langs", required=True, help="Language list seperated with comma")
+    argparser.add_argument("--langs", required=True, help="2-letter language code list seperated with comma")
     args = argparser.parse_args()
 
     accepted_langs = args.langs.split(",")

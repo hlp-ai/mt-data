6cdf7c7663ce55d2514c296c062a4fe00d15e818
liuxf_hust <hlp_ai@sina.com>
2023-05-23 16:08:36 +0800

    Fix normalized name for japanese.

--- yimt_bitext/web/url_language.py
+++ yimt_bitext/web/url_language.py
@@ -31,7 +31,7 @@ class UrlLanguage(object):
         for code in ['chinese', 'chi', 'zh', "zho", "zh-cn", "zh-tw", "cn"]:
             self.code_to_language[code] = 'zh'
         for code in ['japanese', 'ja', 'ja-jp', 'jpn', 'jp']:
-            self.code_to_language[code] = 'jp'
+            self.code_to_language[code] = 'ja'
         for code in ['vietnamese', 'vi', 'vi-vn']:
             self.code_to_language[code] = 'vi'
         for code in ['korean', 'ko', 'ko-kr', "kor"]:

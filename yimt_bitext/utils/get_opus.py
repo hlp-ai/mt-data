c016a077fa2d3e7493fe5c80d89e4f67670343fe
liuxf_hust <hlp_ai@sina.com>
2023-06-06 11:21:35 +0800

    Update get_opus.py.

--- yimt_bitext/utils/get_opus.py
+++ yimt_bitext/utils/get_opus.py
@@ -6,6 +6,7 @@ OPUS_DOWNLOAD_BASE_URL = "https://object.pouta.csc.fi"
 
 
 def download_opus_moses(corpus_name, corpus_version, sl, tl, out_dir="./"):
+    format = "moses"
     corpus_path = "OPUS-" + corpus_name + "/" + corpus_version
     local_moses_file = os.path.join(out_dir, corpus_name + "-" + corpus_version + "-" + sl + "-" + tl + ".txt.zip")
     remote_moses_file = sl + "-" + tl + ".txt.zip"
@@ -14,16 +15,24 @@ def download_opus_moses(corpus_name, corpus_version, sl, tl, out_dir="./"):
 
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36"}
-    BLOCK_SIZE = 1024 * 128
+    BLOCK_SIZE = 1024 * 1024
+    total = 0
 
     try:
         r = requests.get(moses_url, headers=headers, timeout=(15, 15), stream=True, allow_redirects=False)
         if r.status_code != 200:
+            print(moses_url, r.status_code)
             return False
+
+        if 'content-length' in r.headers:
+            content_size = int(r.headers['content-length'])
+            print("{}: {}".format(moses_url, content_size))
+
         with open(local_moses_file, "wb") as f:
             for block in r.iter_content(BLOCK_SIZE):
                 f.write(block)
-                print(".", end="")
+                total += len(block)
+                print("{}: {}".format(moses_url, total))
         return True
     except Exception as e:
         print(moses_url, e)
@@ -31,7 +40,6 @@ def download_opus_moses(corpus_name, corpus_version, sl, tl, out_dir="./"):
         r.close()
 
 
-format = "moses"
 corpus_name = "KDE4"
 corpus_version = "v2"
 sl = "en"

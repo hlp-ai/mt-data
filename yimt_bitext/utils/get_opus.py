76fe081d0ef9472755f4e9fabe2343d460b0b8f0
Xiaofeng Liu <liuxf@hust.edu.cn>
2023-06-06 17:34:05 +0800

    Download corpora specified in file.

--- yimt_bitext/utils/get_opus.py
+++ yimt_bitext/utils/get_opus.py
@@ -1,6 +1,12 @@
 import os
+import sys
+
 import requests
 
+from yimt_bitext.utils.log import get_logger
+
+
+logger = get_logger("opus.log", "mt.opus")
 
 OPUS_DOWNLOAD_BASE_URL = "https://object.pouta.csc.fi"
 
@@ -11,7 +17,7 @@ def download_opus_moses(corpus_name, corpus_version, sl, tl, out_dir="./"):
     local_moses_file = os.path.join(out_dir, corpus_name + "-" + corpus_version + "-" + sl + "-" + tl + ".txt.zip")
     remote_moses_file = sl + "-" + tl + ".txt.zip"
     moses_url = OPUS_DOWNLOAD_BASE_URL + "/" + corpus_path + "/" + format + "/" + remote_moses_file
-    print(moses_url)
+    logger.info(moses_url)
 
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36"}
@@ -21,31 +27,57 @@ def download_opus_moses(corpus_name, corpus_version, sl, tl, out_dir="./"):
     try:
         r = requests.get(moses_url, headers=headers, timeout=(20, 20), stream=True, allow_redirects=False)
         if r.status_code != 200:
-            print(moses_url, r.status_code)
+            logger.info("{}: {}".format(moses_url, r.status_code))
             return False
 
         if 'content-length' in r.headers:
             content_size = int(r.headers['content-length'])
-            print("{}: {:.4f} MB".format(moses_url, content_size/(1024*1024)))
+            logger.info("{}: {:.4f} MB".format(moses_url, content_size/(1024*1024)))
 
         with open(local_moses_file, "wb") as f:
             for block in r.iter_content(BLOCK_SIZE):
                 f.write(block)
                 total += len(block)
-                print("{}: {:.4f} MB".format(moses_url, total/(1024*1024)))
+                logger.info("{}: {:.4f} MB".format(moses_url, total/(1024*1024)))
         return True
     except Exception as e:
-        print(moses_url, e)
+        logger.info("{}: {}".format(moses_url, e))
     finally:
         r.close()
 
     return False
 
 
-corpus_name = "KDE4"
-corpus_version = "v2"
-sl = "en"
-tl = "tr"
+# corpus_name = "KDE4"
+# corpus_version = "v2"
+# sl = "en"
+# tl = "tr"
+#
+# success = download_opus_moses(corpus_name, corpus_version, sl, tl)
+# print(success)
 
-success = download_opus_moses(corpus_name, corpus_version, sl, tl)
-print(success)
+
+if __name__ == "__main__":
+    corpora_list = sys.argv[1]
+    langs_list = sys.argv[2]
+    out_dir = sys.argv[3]
+
+    to_zh = True
+
+    langs = []
+    with open(langs_list, encoding="utf-8") as f:
+        for line in f:
+            langs.append(line.strip())
+
+    with open(corpora_list, encoding="utf-8") as f:
+        for line in f:
+            parts = line.strip().split()
+            corpus_name = parts[0]
+            corpus_version = parts[1]
+
+            if to_zh:
+                for lang in langs:
+                    p = os.path.join(out_dir, lang + "-" + "zh")
+                    os.makedirs(p, exist_ok=True)
+                    success = download_opus_moses(corpus_name, corpus_version, lang, "zh", p)
+                    logger.info(corpus_name + " " + lang + "-zh " + str(success))

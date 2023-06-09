9e9d3d094ed7346c5741184bf4b7d70c01b86353
Xiaofeng Liu <liuxf@hust.edu.cn>
2023-06-09 16:02:16 +0800

    Add utility methods for OPUS.

--- yimt_bitext/opus/utils.py
+++ yimt_bitext/opus/utils.py
@@ -27,3 +27,72 @@ def extract_zips(zips_dir, out_dir=None):
         zFile.close()
 
     return out_dir
+
+
+def same_lines(path1, path2):
+    """Two text files have the same numbers of lines?"""
+    lines1 = 0
+    lines2 = 0
+    with open(path1, encoding="utf-8") as f:
+        for _ in f:
+            lines1 += 1
+
+    with open(path2, encoding="utf-8") as f:
+        for _ in f:
+            lines2 += 1
+
+    if lines1 == lines2:
+        return True
+    else:
+        return False
+
+
+def single_to_pair(src_path, tgt_path, pair_path):
+    """Combine source and target file into a parallel one"""
+    logger_opus.info("{} {}->{}".format(src_path, tgt_path, pair_path))
+    assert same_lines(src_path, tgt_path)
+
+    cnt = 0
+    with open(src_path, encoding="utf-8") as src_f, open(tgt_path, encoding="utf-8") as tgt_f, open(pair_path, "w", encoding="utf-8") as out_f:
+        for p in zip(src_f, tgt_f):
+            out_f.write(p[0].strip() + "\t" + p[1].strip() + "\n")
+            cnt += 1
+            if cnt % 100000 == 0:
+                logger_opus.info("{}: {}".format(pair_path, cnt))
+        logger_opus.info("{}: {}".format(pair_path, cnt))
+
+
+def merge_moses(in_dir, source_lang=None, target_lang=None, out_dir=None):
+    assert source_lang is not None or target_lang is not None
+
+    if out_dir is None:
+        out_dir = os.path.join(in_dir, "tsv")
+
+    files = os.listdir(in_dir)
+    assert len(files)%2 == 0
+
+    files = list(sorted(files))
+
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir, exist_ok=True)
+
+    for i in range(0, len(files), 2):
+        f1 = files[i]
+        f2 = files[i+1]
+        idx = f1.rfind(".")
+        bname = f1[:idx]
+        outf = os.path.join(out_dir, bname + ".tsv")
+        f1 = os.path.join(in_dir, f1)
+        f2 = os.path.join(in_dir, f2)
+        if source_lang is not None:
+            if f1.endswith(source_lang):
+                single_to_pair(f1, f2, outf)
+            elif f2.endswith(source_lang):
+                single_to_pair(f2, f1, outf)
+        elif target_lang is not None:
+            if f1.endswith(target_lang):
+                single_to_pair(f2, f1, outf)
+            elif f2.endswith(target_lang):
+                single_to_pair(f1, f2, outf)
+
+    return out_dir

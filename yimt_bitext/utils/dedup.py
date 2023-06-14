5fa401df97b1b5e91f326ef9a3bf9e863dad3bce
Xiaofeng Liu <liuxf@hust.edu.cn>
2023-06-14 19:27:14 +0800

    Add dedup_bitext_file function.

--- yimt_bitext/utils/dedup.py
+++ yimt_bitext/utils/dedup.py
@@ -32,7 +32,8 @@ def dedup_file(in_path, out_path=None, logger=None):
             pn = norm(p)
             h = hash(pn)
             if h in pairs:
-                logger.debug("Duplicate: {}".format(p))
+                if logger:
+                    logger.debug("Duplicate: {}".format(p))
                 continue
             else:
                 pairs.add(h)
@@ -40,7 +41,77 @@ def dedup_file(in_path, out_path=None, logger=None):
             n += 1
             out_f.write(p + "\n")
 
-    logger.info("Total: {}, Unique: {}".format(total, n))
+    if logger:
+        logger.info("Total: {}, Unique: {}".format(total, n))
+
+    return out_path
+
+
+def dedup_bitext_file(in_path, out_path=None,
+                      dedup_src=True, dedup_tgt=False, dedup_srctgt=False,
+                      lower=True, remove_noletter=True,
+                      logger=None):
+    """Deduplicate bitext file"""
+    pairs = set()
+    srcs = set()
+    tgts = set()
+
+    if out_path is None:
+        out_path = in_path + ".deduped"
+
+    n = 0
+    total = 0
+
+    with open(in_path, encoding="utf-8") as f, open(out_path, "w", encoding="utf-8") as out_f:
+        for p in f:
+            p = p.strip()
+            total += 1
+            if total % 100000 == 0:
+                if logger:
+                    logger.info("Total: {}, Unique: {}".format(total, n))
+
+            if dedup_src or dedup_tgt:
+                pp = p.split("\t")
+                if len(pp) != 2:
+                    continue
+
+                src = pp[0].strip()
+                tgt = pp[1].strip()
+                if dedup_src:
+                    src = norm(src, lower, remove_noletter)
+                    hs = hash(src)
+                    if hs in srcs:
+                        if logger:
+                            logger.debug("Duplicate: {}".format(p))
+                        continue
+                    else:
+                        srcs.add(hs)
+
+                if dedup_tgt:
+                    tgt = norm(tgt, lower, remove_noletter)
+                    ht = hash(tgt)
+                    if ht in tgts:
+                        if logger:
+                            logger.debug("Duplicate: {}".format(p))
+                        continue
+                    else:
+                        tgts.add(ht)
+
+            if dedup_srctgt:
+                pn = norm(p, lower, remove_noletter)
+                h = hash(pn)
+                if h in pairs:
+                    if logger:
+                        logger.debug("Duplicate: {}".format(p))
+                    continue
+                else:
+                    pairs.add(h)
+
+            n += 1
+            out_f.write(p + "\n")
+
+    if logger:
+        logger.info("Total: {}, Unique: {}".format(total, n))
 
     return out_path
 

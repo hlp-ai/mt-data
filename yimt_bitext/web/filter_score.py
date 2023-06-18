b2a75726f31de0e30a7dbe7c3c0016123bb529b3
Xiaofeng Liu <liuxf@hust.edu.cn>
2023-06-18 11:50:43 +0800

    Change filter function into filter_tsv and logger info.

--- yimt_bitext/web/filter_score.py
+++ yimt_bitext/web/filter_score.py
@@ -1,9 +1,15 @@
 import argparse
 
 
-def filter(in_path, out_path, min_score):
+def filter_tsv(in_path, out_path, min_score, logger=None):
+    total = 0
+    left = 0
     with open(in_path, encoding="utf-8") as in_f, open(out_path, "w", encoding="utf-8") as out_f:
         for line in in_f:
+            total += 1
+            if total % 100000 == 0:
+                if logger:
+                    logger.info("Total: {}, Left: {}".format(total, left))
             line = line.strip()
             parts = line.split("\t")
             if len(parts) != 3:
@@ -11,6 +17,10 @@ def filter(in_path, out_path, min_score):
 
             if float(parts[0]) > min_score:
                 out_f.write(parts[1] + "\t" + parts[2] + "\n")
+                left += 1
+
+    if logger:
+        logger.info("Total: {}, Left: {}".format(total, left))
 
 
 if __name__ == "__main__":
@@ -26,4 +36,4 @@ if __name__ == "__main__":
         out_path = in_path + ".sf"
     min_score = args.min
 
-    filter(in_path, out_path, min_score)
+    filter_tsv(in_path, out_path, min_score)

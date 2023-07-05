c9e91634a5ced0121facf4cf9219e1c9cd44c70b
Xiaofeng Liu <liuxf@hust.edu.cn>
2023-07-05 14:07:27 +0800

    Improve merge.py.

--- yimt_bitext/bin/merge.py
+++ yimt_bitext/bin/merge.py
@@ -1,11 +1,14 @@
 import argparse
 
 from yimt_bitext.opus.utils import merge
+from yimt_bitext.utils.log import get_logger
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("in_dir")
-    parser.add_argument("out_fn")
+    parser.add_argument("-i", "--input", nargs="+", required=True, help="input files or directories")
+    parser.add_argument("-o", "--output", required=True, help="output file")
     args = parser.parse_args()
 
-    merge(args.in_dir, args.out_fn)
+    logger = get_logger("./log.txt", "corpus")
+
+    merge(args.input, args.output)

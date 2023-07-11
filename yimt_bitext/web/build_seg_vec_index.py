f933d9c7200f0b3425ccdd24edea9b7b4570a61a
Xiaofeng Liu <liuxf@hust.edu.cn>
2023-07-11 16:05:42 +0800

    Improve scoring with margin score.

--- yimt_bitext/web/build_seg_vec_index.py
+++ yimt_bitext/web/build_seg_vec_index.py
@@ -1,39 +1,48 @@
 import argparse
+from annoy import AnnoyIndex
+from yimt_bitext.web.sentence_vector import SentenceVectorizationLaBSE_2
 
-def build_vec_index(sents_dir, annoy_dir, dim = 768, tree_num = 10):
-    from annoy import AnnoyIndex
-    from yimt_bitext.web.sentence_vector import SentenceVectorizationLaBSE_2
-    segment_vector = SentenceVectorizationLaBSE_2("D:/LaBSE_2",
-                                                  "C:/Users/Lenovo/Desktop/universal-sentence-encoder-cmlm_multilingual-preprocess_2")
-    t = AnnoyIndex(dim, 'angular')
-    block = 100         # 批处理行数
+
+def build_vec_index(sents_dir, segment_vector, annoy_dir=None, dim=768, tree_num=10):
+    if annoy_dir is None:
+        annoy_dir = sents_dir + ".vecindex"
+
+    print("Building index {}".format(annoy_dir))
+
+    vec_index = AnnoyIndex(dim, 'angular')
+    block = 100  # 批处理行数
     segs = []
     i = 0
     j = 0
-    with open(sents_dir, encoding="utf-8") as f:     # 迭代器按行读取文本，而不是整个文本
+    with open(sents_dir, encoding="utf-8") as f:
         for s in f:
             segs.append(s.strip())
             i = i + 1
-            if(i % block == 0):         # 把句子集按block分批放入内存，进行句子嵌入向量，加入索引
+            if i % block == 0:  # 把句子集按block分批放入内存，进行句子嵌入向量，加入索引
                 sentence_embeddings = segment_vector.get_vector(segs)
                 segs = []
                 for v in sentence_embeddings:
-                    t.add_item(j, v)
+                    vec_index.add_item(j, v)
                     j = j + 1
-                print("processed sentence_embeddings:{}".format(j))
-    sentence_embeddings = segment_vector.get_vector(segs)     # 处理末尾小于block大小的句子集段
-    for v in sentence_embeddings:
-        t.add_item(j, v)
-        j = j + 1
-    print("processed sentence_embeddings:{}".format(j))
-    t.build(tree_num)
-    t.save(annoy_dir)
-    print("vec_index has been built successfully")
+                print(j)
+
+    if len(segs) > 0:
+        sentence_embeddings = segment_vector.get_vector(segs)  # 处理末尾小于block大小的句子集段
+        for v in sentence_embeddings:
+            vec_index.add_item(j, v)
+            j = j + 1
+        print(j)
+
+    vec_index.build(tree_num)
+    vec_index.save(annoy_dir)
+
+    return annoy_dir
+
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
     argparser.add_argument("--file", required=True, help="Text file")
-    argparser.add_argument("--annoy_dir", required=True, help="Annoy path for this language")
+    argparser.add_argument("--annoy_dir", default=None, help="Annoy path for this language")
     argparser.add_argument("--dim", default=768, help="The dim of sentence embeddings")
     argparser.add_argument("--tree_num", default=10, help="The number of annoy trees")
     args = argparser.parse_args()
@@ -43,7 +52,7 @@ if __name__ == "__main__":
     dim = args.dim
     tree_num = args.tree_num
 
-    build_vec_index(fn, annoy_dir, dim, tree_num)
+    segment_vector = SentenceVectorizationLaBSE_2("D:/LaBSE_2",
+                                                  "C:/Users/Lenovo/Desktop/universal-sentence-encoder-cmlm_multilingual-preprocess_2")
 
-
-
+    build_vec_index(fn, segment_vector, annoy_dir, dim, tree_num)
--- yimt_bitext/web/score_margin.py
+++ yimt_bitext/web/score_margin.py
@@ -1,67 +1,73 @@
-"""Test Margin_score sentence pairs"""
-def build_vec_index_tsv(tsv_file, indexfile1, indexfile2, dim = 768, tree_num = 10):
-    import os
-    from yimt_bitext.opus.utils import pair_to_single
-    from yimt_bitext.web.build_seg_vec_index import build_vec_index
-    dir_name = os.path.dirname(tsv_file)
-    src_file = os.path.join(dir_name, "tsv_src.txt")
-    tar_file = os.path.join(dir_name, "tsv_tar.txt")
-    pair_to_single(tsv_file, src_file, tar_file)
-    build_vec_index(src_file, indexfile1, dim, tree_num)
-    build_vec_index(tar_file, indexfile2, dim, tree_num)
-    print("Both vec_index has been built successfully")
+import argparse
+from yimt_bitext.opus.utils import pair_to_single
+from yimt_bitext.web.build_seg_vec_index import build_vec_index
+from yimt_bitext.web.sentence_vector import SentenceVectorizationLaBSE_2, load_vec_index, VectorSimilarityMargin, \
+    SentenceVectorizationLaBSE
 
-def score_tsv_margin(tsv_file, output_file, annoy_dir1, annoy_dir2, k = 8, dim = 768):
-    from yimt_bitext.web.sentence_vector import SentenceVectorizationLaBSE_2, load_vec_index, VectorSimilarityMargin
-    segment_vector = SentenceVectorizationLaBSE_2("D:/LaBSE_2",
-                                                  "C:/Users/Lenovo/Desktop/universal-sentence-encoder-cmlm_multilingual-preprocess_2")
+
+def build_vec_index_tsv(tsv_file, segment_vector, indexfile1=None, indexfile2=None, dim=768, tree_num=10):
+    src_file = tsv_file + ".src"
+    tar_file = tsv_file + ".tgt"
+
+    print("Splitting {} into {} and {}".format(tsv_file, src_file, tar_file))
+    pair_to_single(tsv_file, src_file, tar_file)
+
+    index_path1 = build_vec_index(src_file, segment_vector, indexfile1, dim, tree_num)
+    index_path2 = build_vec_index(tar_file, segment_vector, indexfile2, dim, tree_num)
+
+    return index_path1, index_path2
+
+
+def score_tsv_margin(tsv_file, output_file, segment_vector, annoy_dir1, annoy_dir2, k=8, dim=768):
     index1 = load_vec_index(annoy_dir1, dim)
     index2 = load_vec_index(annoy_dir2, dim)
     vec_scorer = VectorSimilarityMargin(index1, index2, k)
 
     block = 100  # 分批进行嵌入和评分，汇报进度
-    i = 1
+    i = 0
     src_segs = []
     tar_segs = []
-    with open(tsv_file, encoding="utf-8") as f:
-        with open(output_file, 'w') as of:
-            # 用迭代器分行读写文件，解决文件过大的问题
-            for s in f:
-                src, tar = s.strip().split('\t')
-                src_segs.append(src)
-                tar_segs.append(tar)
-                if (i % block == 0):
-                    src_vec_segs = segment_vector.get_vector(src_segs)
-                    tar_vec_segs = segment_vector.get_vector(tar_segs)
-                    for j in range(block):
-                        score = vec_scorer.get_score(src_vec_segs[j], tar_vec_segs[j])[0][0]
-                        of.write("{:.6f} {} {}\n".format(score, src_segs[j], tar_segs[j]))
-                    print("scored sentence pairs:{}".format(i))
-                    src_segs = []
-                    tar_segs = []
-                i += 1
+    with open(tsv_file, encoding="utf-8") as f, open(output_file, 'w', encoding="utf-8") as of:
+        for s in f:
+            src, tar = s.strip().split('\t')
+            src_segs.append(src)
+            tar_segs.append(tar)
+            i += 1
+            if i % block == 0:
+                src_vec_segs = segment_vector.get_vector(src_segs)
+                tar_vec_segs = segment_vector.get_vector(tar_segs)
+                for j in range(block):
+                    score = vec_scorer.get_score(src_vec_segs[j], tar_vec_segs[j])[0][0]
+                    of.write("{:.4f} {} {}\n".format(score, src_segs[j], tar_segs[j]))
+
+                src_segs = []
+                tar_segs = []
+                print(i)
+
+        if len(src_segs) > 0:
             src_vec_segs = segment_vector.get_vector(src_segs)
             tar_vec_segs = segment_vector.get_vector(tar_segs)
             for j in range(len(src_segs)):
                 score = vec_scorer.get_score(src_vec_segs[j], tar_vec_segs[j])[0][0]
                 of.write("{:.4f} {} {}\n".format(score, src_segs[j], tar_segs[j]))
-            print("scored sentence pairs:{}".format(i-1))
-    print("All sentence pairs have been scored successfully")
+            print(i)
+
 
 if __name__ == "__main__":
-    import argparse
     argparser = argparse.ArgumentParser()
-    argparser.add_argument("--tsv_file", required=True, help="Input file includes sentence pairs")
-    argparser.add_argument("--output_file", required=True, help="Output file includes scores")
-    argparser.add_argument("--annoy_dir1", required=True, help="Annoy path for language1")
-    argparser.add_argument("--annoy_dir2", required=True, help="Annoy path for language2")
+    argparser.add_argument("--tsv_file", required=True, help="Input tsv file")
+    argparser.add_argument("--output", default=None, help="Output file with scores")
     args = argparser.parse_args()
 
     tsv_file = args.tsv_file
-    output_file = args.output_file
-    annoy_dir1 = args.annoy_dir1
-    annoy_dir2 = args.annoy_dir2
-    k = args.k
-    dim = args.dim
-    # build_vec_index_tsv(tsv_file, annoy_dir1, annoy_dir2)
-    score_tsv_margin(tsv_file, output_file, annoy_dir1, annoy_dir2)
+    output_file = args.output
+
+    # segment_vector = SentenceVectorizationLaBSE_2("D:/LaBSE_2",
+    #                                               "C:/Users/Lenovo/Desktop/universal-sentence-encoder-cmlm_multilingual-preprocess_2")
+    segment_vector = SentenceVectorizationLaBSE("D:/kidden/mt/open/mt-ex/mt/data/labse1")
+
+    index_path1, index_path2 = build_vec_index_tsv(tsv_file, segment_vector)
+
+    if output_file is None:
+        output_file = tsv_file + ".score"
+    score_tsv_margin(tsv_file, output_file, segment_vector, index_path1, index_path2)

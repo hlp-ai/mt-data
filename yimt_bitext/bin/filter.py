e56071369b6dfd82a9bc82c9ecd886e655ce2816
Xiaofeng Liu <liuxf@hust.edu.cn>
2023-07-19 13:18:37 +0800

    Add bin/filter.py.

---
+++ yimt_bitext/bin/filter.py
@@ -0,0 +1,29 @@
+import argparse
+
+from yimt_bitext.utils.filters import get_lang2script, filter_file, EmptyFilter, SameFilter, OverlapFilter, \
+    NonZeroNumeralsFilter, RepetitionFilter, AlphabetRatioFilter, CharacterRatioFilter
+from yimt_bitext.utils.log import get_logger
+
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("--input", required=True, help="Input file path")
+    argparser.add_argument("--output", default=None, help="Ouput file path")
+    argparser.add_argument("--lang_pair", required=True, help="Language pair")
+    args = argparser.parse_args()
+
+    logger = get_logger("./log.txt", "corpus")
+
+    filters = [EmptyFilter(), SameFilter(), OverlapFilter(ratio=0.80), NonZeroNumeralsFilter(threshold=1.0),
+               AlphabetRatioFilter(threshold=0.33, exclude_whitespace=True), RepetitionFilter()]
+
+    lang2script = get_lang2script()
+
+    lang_pair = args.lang_pair
+    sl, tl = lang_pair.split("-")
+    src_script = lang2script[sl]
+    tgt_script = lang2script[tl]
+    char_filter = CharacterRatioFilter(scripts=(src_script, tgt_script), thresholds=(0.33, 0.33))
+    filters.append(char_filter)
+
+    filter_file(args.input, filters=filters, out_path=args.output, logger=logger)

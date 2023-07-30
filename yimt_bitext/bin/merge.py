17037cd575808340db302dd9b7910a9365f62576
Xiaofeng Liu <liuxf@hust.edu.cn>
2023-07-30 19:08:21 +0800

    Add log dir argument for merge.py.

--- yimt_bitext/bin/merge.py
+++ yimt_bitext/bin/merge.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 
 from yimt_bitext.opus.utils import merge
 from yimt_bitext.utils.log import get_logger
@@ -7,8 +8,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input", nargs="+", required=True, help="input files or directories")
     parser.add_argument("-o", "--output", required=True, help="output file")
+    parser.add_argument("--log_dir", default="./", help="log directory")
     args = parser.parse_args()
 
-    logger = get_logger("./log.txt", "corpus")
+    logger_opus = get_logger(os.path.join(args.log_dir, "opus.log"), "OPUS")
 
-    merge(args.input, args.output, logger_opus=logger)
+    merge(args.input, args.output, logger_opus=logger_opus)

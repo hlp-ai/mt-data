ce9b240f8126b5dadc5f590a0bd9daa668b38ae8
Xiaofeng Liu <liuxf@hust.edu.cn>
2023-07-30 18:58:56 +0800

    Add log dir argument for filter_score.py and filter_score_dir.py.

--- yimt_bitext/bin/filter_score.py
+++ yimt_bitext/bin/filter_score.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 
 from yimt_bitext.opus.utils import filter_tsv
 from yimt_bitext.utils.log import get_logger
@@ -9,6 +10,7 @@ if __name__ == "__main__":
     argparser.add_argument("-i", "--input", required=True, help="Bitext file with scores")
     argparser.add_argument("-o", "--output", default=None, help="output file")
     argparser.add_argument("--min", default=0.66, type=float, help="Min socre for bitext")
+    argparser.add_argument("--log_dir", default="./", help="log directory")
     args = argparser.parse_args()
 
     in_path = args.input
@@ -17,6 +19,6 @@ if __name__ == "__main__":
         out_path = in_path + ".sf"
     min_score = args.min
 
-    logger = get_logger("./log.txt", "corpus")
+    logger_opus = get_logger(os.path.join(args.log_dir, "opus.log"), "OPUS")
 
-    filter_tsv(in_path, out_path, min_score, logger=logger)
+    filter_tsv(in_path, out_path, min_score, logger=logger_opus)
--- yimt_bitext/bin/filter_score_dir.py
+++ yimt_bitext/bin/filter_score_dir.py
@@ -9,26 +9,27 @@ if __name__ == "__main__":
     argparser = argparse.ArgumentParser(description="Filter corpus based on score")
     argparser.add_argument("-i", "--input", required=True, help="input directory")
     argparser.add_argument("--min", default=0.66, type=float, help="Min socre for bitext")
+    argparser.add_argument("--log_dir", default="./", help="log directory")
     args = argparser.parse_args()
 
     in_path = args.input
     min_score = args.min
 
-    logger = get_logger("./log.txt", "corpus")
+    logger_opus = get_logger(os.path.join(args.log_dir, "opus.log"), "OPUS")
 
     filter_dir = os.path.join(in_path, "sfilter")
     if not os.path.exists(filter_dir):
         os.mkdir(filter_dir)
     else:
-        logger.info("{} exists".format(filter_dir))
+        logger_opus.info("{} exists".format(filter_dir))
 
     files = os.listdir(in_path)
     for f in files:
         if f.endswith(".score"):
-            logger.info("Filter {} by score".format(f))
+            logger_opus.info("Filter {} by score".format(f))
             out_path = os.path.join(filter_dir, f + ".sfilter")
             if os.path.exists(out_path):
-                logger.info("{} exists".format(out_path))
+                logger_opus.info("{} exists".format(out_path))
                 continue
 
-            filter_tsv(os.path.join(in_path, f), out_path, min_score, logger=logger)
+            filter_tsv(os.path.join(in_path, f), out_path, min_score, logger=logger_opus)

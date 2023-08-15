fb3485b92a59111741b08e587cd1b7934f83b9be
Xiaofeng Liu <liuxf@hust.edu.cn>
2023-08-15 10:40:32 +0800

    Add diff function.

--- yimt_bitext/opus/utils.py
+++ yimt_bitext/opus/utils.py
@@ -436,3 +436,86 @@ def detok_zh_file(in_file, out_file=None):
             outf.write(line + "\n")
 
     outf.close()
+
+
+def diff(tsv_file1, tsv_file2, out_file=None, creterion="SRC",
+             lower=True, remove_noletter=True,
+         logger=None):
+    if out_file is None:
+        out_file = tsv_file1 + ".diffed"
+    pairs = set()
+    srcs = set()
+    tgts = set()
+    total = 0
+
+    if logger:
+        logger.info("Scanning file2...")
+
+    with open(tsv_file2, encoding="utf-8") as bf:
+        for p in bf:
+            total += 1
+            if total % 10000 == 0:
+                if logger:
+                    logger.info("{}: {}".format(tsv_file2, total))
+            p = p.strip()
+            pp = p.split("\t")
+            if len(pp) != 2:
+                continue
+            src = pp[0].strip()
+            tgt = pp[1].strip()
+            src = norm(src, lower, remove_noletter)
+            hs = hash(src)
+            srcs.add(hs)
+
+            tgt = norm(tgt, lower, remove_noletter)
+            ht = hash(tgt)
+            tgts.add(ht)
+
+            p = norm(p, lower, remove_noletter)
+            h = hash(p)
+            pairs.add(h)
+
+    if logger:
+        logger.info("{}: {}".format(tsv_file2, total))
+
+    differed = 0
+    total = 0
+
+    if logger:
+        logger.info("Scanning file1...")
+
+    with open(tsv_file1, encoding="utf-8") as f, open(out_file, "w", encoding="utf-8") as out_f:
+        for p in f:
+            p = p.strip()
+            total += 1
+            if total % 100000 == 0:
+                if logger:
+                    logger.info("Total: {}, Difference: {}".format(total, differed))
+
+            if creterion == "SRC" or creterion == "TGT":
+                pp = p.split("\t")
+                if len(pp) != 2:
+                    continue
+                src = pp[0].strip()
+                tgt = pp[1].strip()
+                if creterion == "SRC":
+                    src = norm(src, lower, remove_noletter)
+                    hs = hash(src)
+                    if hs not in srcs:
+                        out_f.write(p + "\n")
+                        differed += 1
+                else:
+                    tgt = norm(tgt, lower, remove_noletter)
+                    ht = hash(tgt)
+                    if ht not in tgts:
+                        out_f.write(p + "\n")
+                        differed += 1
+            else:
+                pn = norm(p, lower, remove_noletter)
+                h = hash(pn)
+                if h not in pairs:
+                    out_f.write(p + "\n")
+                    differed += 1
+
+    if logger:
+        logger.info("Total: {}, Difference: {}".format(total, differed))

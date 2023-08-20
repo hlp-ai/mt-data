4c165267bafae0ee56e1e295cec98896a4b955e5
Xiaofeng Liu <liuxf@hust.edu.cn>
2023-08-20 11:12:20 +0800

    Extract strip_scores function.

--- yimt_bitext/bin/remove_scores.py
+++ yimt_bitext/bin/remove_scores.py
@@ -1,16 +1,17 @@
 import sys
 
 
-if __name__ == "__main__":
-    in_path = sys.argv[1]
-    out_path  = in_path + "-noscore"
+def strip_scores(in_path, out_path=None, logger=None):
+    if out_path is None:
+        out_path = in_path + "-noscore"
 
     with open(in_path, encoding="utf-8") as in_f, open(out_path, "w", encoding="utf-8") as out_f:
-        total  = 0
+        total = 0
         for line in in_f:
             total += 1
             if total % 100000 == 0:
-                print(total)
+                if logger:
+                    logger.info("{}".format(total))
             line = line.strip()
             parts = line.split("\t")
             if len(parts) != 3:
@@ -18,4 +19,12 @@ if __name__ == "__main__":
 
             out_f.write(parts[1] + "\t" + parts[2] + "\n")
 
-        print(total)
+        if logger:
+            logger.info("{}".format(total))
+
+
+if __name__ == "__main__":
+    in_path = sys.argv[1]
+    out_path  = in_path + "-noscore"
+
+    strip_scores(in_path)

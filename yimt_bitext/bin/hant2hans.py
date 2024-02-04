c3f3c88990f931b23c164939de691ae3920af9f7
Xiaofeng Liu <liuxf@hust.edu.cn>
2024-02-04 16:15:40 +0800

    文件读写自动关闭；main部分调用方法完成。

--- yimt_bitext/bin/hant2hans.py
+++ yimt_bitext/bin/hant2hans.py
@@ -1,27 +1,23 @@
-"""Convert file in traditional Chinese into file in simplified Chinese"""
+"""将繁体中文转换成简体中文"""
 import argparse
-import io
 
 from yimt_bitext.utils.normalizers import hant_2_hans
 
 
 def hant2s_file(in_fn, out_fn):
-    in_f = io.open(in_fn, encoding="utf-8")
-    out_f = io.open(out_fn, "w", encoding="utf-8")
-
     cnt = 0
-    for line in in_f:
-        line = line.strip()
-        line_s = hant_2_hans(line)
-        out_f.write(line_s + "\n")
+    with open(in_fn, encoding="utf-8") as in_f, open(out_fn, "w", encoding="utf-8") as out_f:
+        for line in in_f:
+            line = line.strip()
+            line_s = hant_2_hans(line)
+            out_f.write(line_s + "\n")
 
-        cnt += 1
-        if cnt % 100000 == 0:
-            print(cnt)
-
-    out_f.close()
+            cnt += 1
+            if cnt % 100000 == 0:
+                print(cnt)
 
     print(cnt)
+
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
@@ -29,17 +25,4 @@ if __name__ == "__main__":
     argparser.add_argument("out_fn")
     args = argparser.parse_args()
 
-    in_f = io.open(args.in_fn, encoding="utf-8")
-    out_f = io.open(args.out_fn, "w", encoding="utf-8")
-
-    cnt = 0
-    for line in in_f:
-        line = line.strip()
-        line_s = hant_2_hans(line)
-        out_f.write(line_s + "\n")
-
-        cnt += 1
-        if cnt % 100000 == 0:
-            print(cnt)
-
-    print(cnt)
+    hant2s_file(args.in_fn, args.out_fn)

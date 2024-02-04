8a55035d57c6871f6e607eaaeffbe89e275824d9
Xiaofeng Liu <liuxf@hust.edu.cn>
2024-02-04 16:29:18 +0800

    移动merge_moses.py到opus包中。

---
+++ yimt_bitext/opus/merge_moses.py
@@ -0,0 +1,73 @@
+"""合并给定目录下各个语料的单语文件为平行语料TSV文件"""
+import argparse
+import os
+
+from yimt_bitext.opus.utils import single_to_pair
+from yimt_bitext.utils.log import get_logger
+from yimt_bitext.utils.normalizers import detok_zh_file_inplace
+
+
+def merge_moses(in_dir, source_lang=None, target_lang=None, out_dir=None,
+                clean_after_merge=False,logger_opus=None):
+    assert source_lang is not None or target_lang is not None
+
+    if out_dir is None:
+        out_dir = os.path.join(in_dir, "tsv")
+
+    if os.path.exists(out_dir):
+        logger_opus.info("{} exits".format(out_dir))
+        return out_dir
+
+    files = os.listdir(in_dir)
+    assert len(files)%2 == 0
+
+    files = list(sorted(files))
+
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir, exist_ok=True)
+
+    for i in range(0, len(files), 2):
+        f1 = files[i]
+        f2 = files[i+1]
+        idx = f1.rfind(".")
+        bname = f1[:idx]
+        outf = os.path.join(out_dir, bname + ".tsv")
+        f1 = os.path.join(in_dir, f1)
+        f2 = os.path.join(in_dir, f2)
+
+        if f1.endswith("zh") and (f1.find("bible") >= 0 or f1.find("XLEnt") >= 0 or f1.find("SPC") >= 0) :
+            logger_opus.info("Detokenizing {}".format(f1))
+            detok_zh_file_inplace(f1)
+        elif f2.endswith("zh") and (f2.find("bible") >= 0 or f2.find("XLEnt") >= 0 or f2.find("SPC") >= 0):
+            logger_opus.info("Detokenizing {}".format(f2))
+            detok_zh_file_inplace(f2)
+
+        if source_lang is not None:
+            if f1.endswith(source_lang):
+                single_to_pair(f1, f2, outf, logger_opus)
+            elif f2.endswith(source_lang):
+                single_to_pair(f2, f1, outf, logger_opus)
+        elif target_lang is not None:
+            if f1.endswith(target_lang):
+                single_to_pair(f2, f1, outf, logger_opus)
+            elif f2.endswith(target_lang):
+                single_to_pair(f1, f2, outf, logger_opus)
+
+        if clean_after_merge:
+            os.remove(f1)
+            os.remove(f2)
+
+    return out_dir
+
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("--in_dir", type=str, required=True, help="input dir")
+    argparser.add_argument("--out_dir", type=str, default=None, help="output dir")
+    argparser.add_argument("--src_lang", type=str, default=None, help="source language")
+    argparser.add_argument("--tgt_lang", type=str, default=None, help="target language")
+    args = argparser.parse_args()
+
+    logger = get_logger("./log.txt", "corpus")
+
+    merge_moses(args.in_dir, args.src_lang, args.tgt_lang, args.out_dir, logger_opus=logger)

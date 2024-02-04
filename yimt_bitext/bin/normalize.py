6b5f54a9cf8afd9c1fa2debe3c50b2fcb6796664
Xiaofeng Liu <liuxf@hust.edu.cn>
2024-02-04 16:26:16 +0800

    normalize.py支持规范化配置文件。

--- yimt_bitext/bin/normalize.py
+++ yimt_bitext/bin/normalize.py
@@ -1,21 +1,19 @@
-"""Normalize bitext"""
+"""文件文本规范化"""
 import argparse
 
 from yimt_bitext.utils.log import get_logger
-from yimt_bitext.utils.normalizers import normalize_file, ToZhNormalizer, CleanerTSV
+from yimt_bitext.utils.normalizers import normalize_file, load_normalizers
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
     argparser.add_argument("--input", required=True, help="Input file path")
     argparser.add_argument("--output", default=None, help="Ouput file path")
-    argparser.add_argument("--tgt_lang", default="zh", help="Target language")
+    argparser.add_argument("--config", required=True, help="Normalizer config file")
     args = argparser.parse_args()
 
     logger = get_logger("./log.txt", "corpus")
 
-    if args.tgt_lang == "zh":
-        normalizers = [ToZhNormalizer()]
-    else:
-        normalizers = [CleanerTSV()]
+    logger.info("Loading normalizer config file: {}".format(args.config))
+    normalizers = load_normalizers(args.config)
 
     normalize_file(args.input, normalizers, out_path=args.output, logger=logger)

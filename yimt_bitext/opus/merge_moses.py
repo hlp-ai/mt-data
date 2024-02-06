4ba7771092ca935108693d3256647a9a3601f0b4
Xiaofeng Liu <liuxf@hust.edu.cn>
2024-02-06 10:13:58 +0800

    改进命令行参数。

--- yimt_bitext/opus/merge_moses.py
+++ yimt_bitext/opus/merge_moses.py
@@ -62,12 +62,12 @@ def merge_moses(in_dir, source_lang=None, target_lang=None, out_dir=None,
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
-    argparser.add_argument("--in_dir", type=str, required=True, help="input dir")
-    argparser.add_argument("--out_dir", type=str, default=None, help="output dir")
-    argparser.add_argument("--src_lang", type=str, default=None, help="source language")
-    argparser.add_argument("--tgt_lang", type=str, default=None, help="target language")
+    argparser.add_argument("-i", "--input", type=str, required=True, help="input dir")
+    argparser.add_argument("-o", "--output", type=str, default=None, help="output dir")
+    argparser.add_argument("-sl", "--src_lang", type=str, default=None, help="source language")
+    argparser.add_argument("-tl", "--tgt_lang", type=str, default=None, help="target language")
     args = argparser.parse_args()
 
-    logger = get_logger("./log.txt", "corpus")
+    logger = get_logger("./log.txt", "OPUS")
 
-    merge_moses(args.in_dir, args.src_lang, args.tgt_lang, args.out_dir, logger_opus=logger)
+    merge_moses(args.input, args.src_lang, args.tgt_lang, args.output, logger_opus=logger)

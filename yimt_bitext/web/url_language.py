fdc6c5ffa1008c5444a9e9239507a63f038aa903
Xiaofeng Liu <liuxf@hust.edu.cn>
2024-02-18 16:27:53 +0800

    增加语言代码映射；基于语言代码映射文件实现UrlLanguage。

--- yimt_bitext/web/url_language.py
+++ yimt_bitext/web/url_language.py
@@ -3,144 +3,151 @@ import re
 
 class UrlLanguage(object):
 
-    def __init__(self, strip_query_variables=False):
+    def __init__(self, strip_query_variables=False, map_file="./langs-map.txt"):
         self._strip_query_variables = []
         if strip_query_variables:
             self._strip_query_variables = [
                 'lang', 'clang', 'language', 'locale', 'selectedLocale']
         self.code_to_language = {}
-        # These should all be lower-case, matching is case-insensitive
-        for code in ['arabic', 'ara', 'ar']:
-            self.code_to_language[code] = 'ar'
-        for code in ['bulgarian', 'bul', 'bg']:
-            self.code_to_language[code] = 'bg'
-        for code in ['breton', 'bre', 'br']:
-            self.code_to_language[code] = 'br'
-        for code in ['czech', 'cze', 'cz', 'cs']:
-            self.code_to_language[code] = 'cs'
-        for code in ['danish', 'da-dk', 'dan', 'da']:
-            self.code_to_language[code] = 'da'
-        for code in ['deutsch', 'german', 'ger', 'deu', 'de-at', 'de-ch', 'de-de', 'de-lu', 'de']:
-            self.code_to_language[code] = 'de'
-        for code in ['english', 'eng', 'en']:
-            self.code_to_language[code] = 'en'
-        for code in ['espanol', 'spanish', 'spa', 'esp', 'es']:
-            self.code_to_language[code] = 'es'
-        for code in ['french', 'francais', 'fran', 'fra', 'fre', 'fr']:
-            self.code_to_language[code] = 'fr'
-        for code in ['chinese', 'chi', 'zh', "zho", "zh-cn", "zh-tw", "cn"]:
-            self.code_to_language[code] = 'zh'
-        for code in ['japanese', 'ja', 'ja-jp', 'jpn', 'jp']:
-            self.code_to_language[code] = 'ja'
-        for code in ['vietnamese', 'vi', 'vi-vn']:
-            self.code_to_language[code] = 'vi'
-        for code in ['korean', 'ko', 'ko-kr', "kor"]:
-            self.code_to_language[code] = 'ko'
-        for code in ['malay', 'ms']:
-            self.code_to_language[code] = 'ms'
-        for code in ['tedesco', 'de-de', 'de-ch', 'de-at', 'de-li', 'de-lu',
-                     'allemand']:
-            self.code_to_language[code] = 'de'
-        for code in ['fr-be', 'fr-ca', 'fr-fr', 'fr-lu', 'fr-ch', 'fr']:  # 法语
-            self.code_to_language[code] = 'fr'
-        for code in ['italian', 'italiano', 'ital', 'ita', 'it-it', 'it-ch',
-                     'it']:
-            self.code_to_language[code] = 'it'
-        for code in ['portuguese', 'portugues', 'pt-pt', 'pt-br', 'ptg', 'ptb', 'pt']:
-            self.code_to_language[code] = 'pt'
-        for code in ['russian', 'russkiy', 'ru-ru', 'rus', 'ru']:
-            self.code_to_language[code] = 'ru'
-        for code in ['dutch', 'nederlands', 'nl-nl', 'nld', 'dut', 'nl']:
-            self.code_to_language[code] = 'nl'
-        for code in ['en-en', 'en-us', 'en-uk', 'en-ca', 'en-bz', 'en-ab',
-                     'en-in', 'en-ie', 'en-jm', 'en-nz', 'en-ph', 'en-za',
-                     'en-tt', 'gb', 'en-gb', 'inglese', 'english', 'us']:
-            self.code_to_language[code] = 'en'
-        for code in ['romanian', 'romana', 'romlang', 'rom', 'ro-ro', 'ro']:
-            self.code_to_language[code] = 'ro'
-        for code in ['soma', 'som', 'so', 'somal', 'somali', 'so-so',
-                     'af-soomaali', 'soomaali']:
-            self.code_to_language[code] = 'so'
-        for code in ['turkish', 'tur', 'turkic', 'tr-tr', 'tr']:
-            self.code_to_language[code] = 'tr'
-        for code in ['finnish', 'finnisch', 'fin', 'suomi', 'suomeksi',
-                     'suominen', 'suomija', 'fi-fi', 'fi']:
-            self.code_to_language[code] = 'fi'
-        for code in ['thailand', 'th-th', 'thai', 'th']:
-            self.code_to_language[code] = 'th'
-        for code in ['greek', 'el', 'ell', 'gre', 'el_gr']:  # 现代希腊语
-            self.code_to_language[code] = 'el'
-        for code in ['latin', 'la', 'lat']:  # 拉丁语
-            self.code_to_language[code] = 'la'
-        for code in ['norwegian', 'Norsk', 'no', 'nor', 'no_no', 'no_no_ny']:
-            self.code_to_language[code] = 'no'
-        for code in ['polish', 'pl', 'pol', 'pl_pl']:  # 波兰语
-            self.code_to_language[code] = 'pl'
-        for code in ['swedish', 'svenska', 'sv', 'swe', 'sv_se']:  # 瑞典语
-            self.code_to_language[code] = 'sv'
-        for code in ['ukrainian', 'uk', 'ukr', 'uk_ua']:  # 乌克兰语
-            self.code_to_language[code] = 'uk'
-        for code in ['burmese', 'my', 'mya', 'bur']:  # 缅甸语
-            self.code_to_language[code] = 'my'
-        for code in ['tibetan', 'bo', 'bod', 'tib']:  # 藏语
-            self.code_to_language[code] = 'bo'
-        for code in ['hungarian', 'hu', 'hun']:  # 匈牙利语
-            self.code_to_language[code] = 'hu'
-        for code in ['indonesian', 'id', 'ind']:  # 印尼语
-            self.code_to_language[code] = 'id'
-        for code in ['mongolian', 'mn', 'mon']:  # 蒙古语
-            self.code_to_language[code] = 'mn'
-        for code in ['belarusian', 'be', 'bel', 'be_by']:  # 白俄罗斯语
-            self.code_to_language[code] = 'be'
-        for code in ['bengali', 'bn', 'ben']:  # 孟加拉语
-            self.code_to_language[code] = 'bn'
-        for code in ['croatian', 'hr', 'hrv', 'hr_hr']:  # 克罗地亚语
-            self.code_to_language[code] = 'hr'
 
-        for code in ['macedonian', 'mk', 'mkd', 'mac']:  # 马其顿语
-            self.code_to_language[code] = 'mk'
-        for code in ['lithuanian', 'lt', 'lit']:  # 立陶宛语
-            self.code_to_language[code] = 'lt'
-        for code in ['estonian', 'et', 'est']:  # 爱沙尼亚语
-            self.code_to_language[code] = 'et'
-        for code in ['latvian', 'lv', 'lav']:   # 拉脱维亚语
-            self.code_to_language[code] = 'lv'
-        for code in ['bulgarian', 'bg', 'bul']:   # 保加利亚语
-            self.code_to_language[code] = 'bg'
-        for code in ['icelandic', 'is', 'isl', 'ice']:   # 冰岛语
-            self.code_to_language[code] = 'is'
-        for code in ['slovenian', 'sl', 'slv']:   # 斯洛文尼亚语
-            self.code_to_language[code] = 'sl'
-        for code in ['slovak', 'sl', 'slk', 'slo']:   # 斯洛伐克语
-            self.code_to_language[code] = 'sk'
-        for code in ['hebrew', 'he', 'heb']:   # 希伯来语
-            self.code_to_language[code] = 'he'
-        for code in ['sinhala', 'sinhalese', 'si', 'sin']:   # 僧加罗语
-            self.code_to_language[code] = 'si'
-        for code in ['afrikaans', 'af', 'afr']:   # 南非语
-            self.code_to_language[code] = 'af'
-        for code in ['xhosa', 'xh', 'xho']:   # 科撒语
-            self.code_to_language[code] = 'xh'
-        for code in ['tagalog', 'tl', 'tgl']:   # 他加禄语
-            self.code_to_language[code] = 'tl'
-        for code in ['malagasy', 'ml', 'mlg']:   # 马达加斯加语
-            self.code_to_language[code] = 'ml'
-        for code in ['hausa', 'ha', 'hau']:   # 豪萨语
-            self.code_to_language[code] = 'ha'
-        for code in ['tamil', 'ta', 'tam']:   # 泰米尔语
-            self.code_to_language[code] = 'ta'
-        for code in ['panjabi', 'pa', 'pan']:   # 旁遮普语
-            self.code_to_language[code] = 'pa'
-        for code in ['tegula', 'te', 'tel']:   # 泰卢固语
-            self.code_to_language[code] = 'te'
-        for code in ['marathi', 'mr', 'mar']:   # 马拉提语
-            self.code_to_language[code] = 'mr'
-        for code in ['swahili', 'sw', 'swa']:   # 斯瓦西里语
-            self.code_to_language[code] = 'sw'
-        for code in ['urdu', 'ur', 'urd']:   # 乌尔都语
-            self.code_to_language[code] = 'ur'
-        for code in ['lao', 'lo', 'laos', 'la']:   # 老挝语
-            self.code_to_language[code] = 'lo'
+        with open(map_file, encoding="utf-8") as f:
+            for line in f:
+                line = line.strip()
+                parts = line.split(",")
+                for i in range(len(parts)):
+                    self.code_to_language[parts[i]] = parts[0]
+        # # These should all be lower-case, matching is case-insensitive
+        # for code in ['arabic', 'ara', 'ar']:
+        #     self.code_to_language[code] = 'ar'
+        # for code in ['bulgarian', 'bul', 'bg']:
+        #     self.code_to_language[code] = 'bg'
+        # for code in ['breton', 'bre', 'br']:
+        #     self.code_to_language[code] = 'br'
+        # for code in ['czech', 'cze', 'cz', 'cs']:
+        #     self.code_to_language[code] = 'cs'
+        # for code in ['danish', 'da-dk', 'dan', 'da']:
+        #     self.code_to_language[code] = 'da'
+        # for code in ['deutsch', 'german', 'ger', 'deu', 'de-at', 'de-ch', 'de-de', 'de-lu', 'de']:
+        #     self.code_to_language[code] = 'de'
+        # for code in ['english', 'eng', 'en']:
+        #     self.code_to_language[code] = 'en'
+        # for code in ['espanol', 'spanish', 'spa', 'esp', 'es']:
+        #     self.code_to_language[code] = 'es'
+        # for code in ['french', 'francais', 'fran', 'fra', 'fre', 'fr']:
+        #     self.code_to_language[code] = 'fr'
+        # for code in ['chinese', 'chi', 'zh', "zho", "zh-cn", "zh-tw", "cn"]:
+        #     self.code_to_language[code] = 'zh'
+        # for code in ['japanese', 'ja', 'ja-jp', 'jpn', 'jp']:
+        #     self.code_to_language[code] = 'ja'
+        # for code in ['vietnamese', 'vi', 'vi-vn']:
+        #     self.code_to_language[code] = 'vi'
+        # for code in ['korean', 'ko', 'ko-kr', "kor"]:
+        #     self.code_to_language[code] = 'ko'
+        # for code in ['malay', 'ms']:
+        #     self.code_to_language[code] = 'ms'
+        # for code in ['tedesco', 'de-de', 'de-ch', 'de-at', 'de-li', 'de-lu',
+        #              'allemand']:
+        #     self.code_to_language[code] = 'de'
+        # for code in ['fr-be', 'fr-ca', 'fr-fr', 'fr-lu', 'fr-ch', 'fr']:  # 法语
+        #     self.code_to_language[code] = 'fr'
+        # for code in ['italian', 'italiano', 'ital', 'ita', 'it-it', 'it-ch',
+        #              'it']:
+        #     self.code_to_language[code] = 'it'
+        # for code in ['portuguese', 'portugues', 'pt-pt', 'pt-br', 'ptg', 'ptb', 'pt']:
+        #     self.code_to_language[code] = 'pt'
+        # for code in ['russian', 'russkiy', 'ru-ru', 'rus', 'ru']:
+        #     self.code_to_language[code] = 'ru'
+        # for code in ['dutch', 'nederlands', 'nl-nl', 'nld', 'dut', 'nl']:
+        #     self.code_to_language[code] = 'nl'
+        # for code in ['en-en', 'en-us', 'en-uk', 'en-ca', 'en-bz', 'en-ab',
+        #              'en-in', 'en-ie', 'en-jm', 'en-nz', 'en-ph', 'en-za',
+        #              'en-tt', 'gb', 'en-gb', 'inglese', 'english', 'us']:
+        #     self.code_to_language[code] = 'en'
+        # for code in ['romanian', 'romana', 'romlang', 'rom', 'ro-ro', 'ro']:
+        #     self.code_to_language[code] = 'ro'
+        # for code in ['soma', 'som', 'so', 'somal', 'somali', 'so-so',
+        #              'af-soomaali', 'soomaali']:
+        #     self.code_to_language[code] = 'so'
+        # for code in ['turkish', 'tur', 'turkic', 'tr-tr', 'tr']:
+        #     self.code_to_language[code] = 'tr'
+        # for code in ['finnish', 'finnisch', 'fin', 'suomi', 'suomeksi',
+        #              'suominen', 'suomija', 'fi-fi', 'fi']:
+        #     self.code_to_language[code] = 'fi'
+        # for code in ['thailand', 'th-th', 'thai', 'th']:
+        #     self.code_to_language[code] = 'th'
+        # for code in ['greek', 'el', 'ell', 'gre', 'el_gr']:  # 现代希腊语
+        #     self.code_to_language[code] = 'el'
+        # for code in ['latin', 'la', 'lat']:  # 拉丁语
+        #     self.code_to_language[code] = 'la'
+        # for code in ['norwegian', 'Norsk', 'no', 'nor', 'no_no', 'no_no_ny']:
+        #     self.code_to_language[code] = 'no'
+        # for code in ['polish', 'pl', 'pol', 'pl_pl']:  # 波兰语
+        #     self.code_to_language[code] = 'pl'
+        # for code in ['swedish', 'svenska', 'sv', 'swe', 'sv_se']:  # 瑞典语
+        #     self.code_to_language[code] = 'sv'
+        # for code in ['ukrainian', 'uk', 'ukr', 'uk_ua']:  # 乌克兰语
+        #     self.code_to_language[code] = 'uk'
+        # for code in ['burmese', 'my', 'mya', 'bur']:  # 缅甸语
+        #     self.code_to_language[code] = 'my'
+        # for code in ['tibetan', 'bo', 'bod', 'tib']:  # 藏语
+        #     self.code_to_language[code] = 'bo'
+        # for code in ['hungarian', 'hu', 'hun']:  # 匈牙利语
+        #     self.code_to_language[code] = 'hu'
+        # for code in ['indonesian', 'id', 'ind']:  # 印尼语
+        #     self.code_to_language[code] = 'id'
+        # for code in ['mongolian', 'mn', 'mon']:  # 蒙古语
+        #     self.code_to_language[code] = 'mn'
+        # for code in ['belarusian', 'be', 'bel', 'be_by']:  # 白俄罗斯语
+        #     self.code_to_language[code] = 'be'
+        # for code in ['bengali', 'bn', 'ben']:  # 孟加拉语
+        #     self.code_to_language[code] = 'bn'
+        # for code in ['croatian', 'hr', 'hrv', 'hr_hr']:  # 克罗地亚语
+        #     self.code_to_language[code] = 'hr'
+        #
+        # for code in ['macedonian', 'mk', 'mkd', 'mac']:  # 马其顿语
+        #     self.code_to_language[code] = 'mk'
+        # for code in ['lithuanian', 'lt', 'lit']:  # 立陶宛语
+        #     self.code_to_language[code] = 'lt'
+        # for code in ['estonian', 'et', 'est']:  # 爱沙尼亚语
+        #     self.code_to_language[code] = 'et'
+        # for code in ['latvian', 'lv', 'lav']:   # 拉脱维亚语
+        #     self.code_to_language[code] = 'lv'
+        # for code in ['bulgarian', 'bg', 'bul']:   # 保加利亚语
+        #     self.code_to_language[code] = 'bg'
+        # for code in ['icelandic', 'is', 'isl', 'ice']:   # 冰岛语
+        #     self.code_to_language[code] = 'is'
+        # for code in ['slovenian', 'sl', 'slv']:   # 斯洛文尼亚语
+        #     self.code_to_language[code] = 'sl'
+        # for code in ['slovak', 'sl', 'slk', 'slo']:   # 斯洛伐克语
+        #     self.code_to_language[code] = 'sk'
+        # for code in ['hebrew', 'he', 'heb']:   # 希伯来语
+        #     self.code_to_language[code] = 'he'
+        # for code in ['sinhala', 'sinhalese', 'si', 'sin']:   # 僧加罗语
+        #     self.code_to_language[code] = 'si'
+        # for code in ['afrikaans', 'af', 'afr']:   # 南非语
+        #     self.code_to_language[code] = 'af'
+        # for code in ['xhosa', 'xh', 'xho']:   # 科撒语
+        #     self.code_to_language[code] = 'xh'
+        # for code in ['tagalog', 'tl', 'tgl']:   # 他加禄语
+        #     self.code_to_language[code] = 'tl'
+        # for code in ['malagasy', 'ml', 'mlg']:   # 马达加斯加语
+        #     self.code_to_language[code] = 'ml'
+        # for code in ['hausa', 'ha', 'hau']:   # 豪萨语
+        #     self.code_to_language[code] = 'ha'
+        # for code in ['tamil', 'ta', 'tam']:   # 泰米尔语
+        #     self.code_to_language[code] = 'ta'
+        # for code in ['panjabi', 'pa', 'pan']:   # 旁遮普语
+        #     self.code_to_language[code] = 'pa'
+        # for code in ['tegula', 'te', 'tel']:   # 泰卢固语
+        #     self.code_to_language[code] = 'te'
+        # for code in ['marathi', 'mr', 'mar']:   # 马拉提语
+        #     self.code_to_language[code] = 'mr'
+        # for code in ['swahili', 'sw', 'swa']:   # 斯瓦西里语
+        #     self.code_to_language[code] = 'sw'
+        # for code in ['urdu', 'ur', 'urd']:   # 乌尔都语
+        #     self.code_to_language[code] = 'ur'
+        # for code in ['lao', 'lo', 'laos', 'la']:   # 老挝语
+        #     self.code_to_language[code] = 'lo'
 
         pairs = list(self.code_to_language.items())
 

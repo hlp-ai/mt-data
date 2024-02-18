2d6a4275c7075eba1d6289e8289dc9ac145954d3
Xiaofeng Liu <liuxf@hust.edu.cn>
2024-02-18 16:05:20 +0800

    增加语言代码映射；保存总的站点集合。

--- yimt_bitext/web/sites_from_stat_group.py
+++ yimt_bitext/web/sites_from_stat_group.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 import os
+from collections import defaultdict
 
 from yimt_bitext.web.sites_from_stat import sites_from_stat
 
@@ -16,6 +17,7 @@ if __name__ == "__main__":
     langs = args.langs.split(",")
 
     total_domains_found = 0
+    domain2hosts_langs_total = defaultdict(list)
 
     for d in dirs:
         meta_dir = os.path.join(group_meta_dir, d)
@@ -26,12 +28,22 @@ if __name__ == "__main__":
 
         d = os.path.dirname(stat_f)
         out_f = os.path.join(d, "sites-" + "-".join(langs) + ".json")
-
         with open(out_f, "w", encoding="utf-8") as f:
-            json.dump(domain2hosts_langs, f)
+            json.dump(domain2hosts_langs, f, indent=2)
 
         print("# of domains found: {}".format(len(domain2hosts_langs)))
+
+        for domain, hosts in domain2hosts_langs.items():
+            if domain not in domain2hosts_langs_total:
+                domain2hosts_langs_total[domain] = hosts
+            else:
+                for host in hosts:
+                    if host not in domain2hosts_langs_total[domain]:
+                        domain2hosts_langs_total[domain].append(host)
 
         total_domains_found += len(domain2hosts_langs)
 
     print("总共发现符合条件的多语域名数: {}".format(total_domains_found))
+    out_f = os.path.join(os.path.dirname(group_meta_dir), "sites-" + "-".join(langs) + ".json")
+    with open(out_f, "w", encoding="utf-8") as f:
+        json.dump(domain2hosts_langs_total, f, indent=2)

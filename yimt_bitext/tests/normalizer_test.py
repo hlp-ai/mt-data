61ea3c419be6f2bcaec275b8b45c61c3b31661cd
Xiaofeng Liu <liuxf@hust.edu.cn>
2024-06-18 15:54:54 +0800

    添加html实体规范化例子。

--- yimt_bitext/tests/normalizer_test.py
+++ yimt_bitext/tests/normalizer_test.py
@@ -1,5 +1,9 @@
-from yimt_bitext.normalize.normalizers import Cleaner
+from yimt_bitext.normalize.normalizers import Cleaner, HTMLEntityUnescape
 
 cleaner = Cleaner()
 print(cleaner.normalize("a  bc\tthis is a test."))
 print(cleaner.normalize("\u0001 a bc\t this ia中文"))
+
+unescape = HTMLEntityUnescape(True, True)
+p = "i like&nbsp;\t&nbsp do."
+print(unescape.normalize(p))
